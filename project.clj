b243e191a09911b2f3af7568f6bcd2add2f2cfa6
Joe Wass <jwass@crossref.org>
2018-03-28 11:52:15 +0100

    Specify RAM constraints.

--- project.clj
+++ project.clj
@@ -4,11 +4,14 @@
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [event-data-common "0.1.51"]
+                 [event-data-common "0.1.52"]
                  [robert/bruce "0.8.0"]
                  [http-kit "2.3.0-alpha5"]
                  [org.clojure/data.json "0.2.6"]
-                 [clj-http-fake "1.0.3"]]
+                 [clj-http-fake "1.0.3"]
+                 [throttler "1.0.0"]]
   :main ^:skip-aot event-data-investigator.core
   :target-path "target/%s"
+  ; On 2017-11-11, with 6.7 million Events, 3.5G was used with no pressure.
+  :jvm-opts ["-Duser.timezone=UTC" "-Xmx4G"]
   :profiles {:uberjar {:aot :all}})
